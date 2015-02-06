7dfc5d549124c0e39328f66fc8ae716379c53c3a
Joshua Madin <joshua.madin@mq.edu.au>
2015-02-06 14:10:32 +1100

    validation for degree type.

--- app/models/degree_type.rb
+++ app/models/degree_type.rb
@@ -1,5 +1,6 @@
 class DegreeType < ActiveRecord::Base
   has_many :degrees, :dependent => :restrict
+  validates :degree_type_name, :presence => true
 
   default_scope -> { order('degree_type_name ASC') }
 
