c05d64d72d540496be26377725ddebbf96f50eee
Joshua Madin <joshua.madin@mq.edu.au>
2016-10-25 16:55:45 +1100

    Added alumni download to CSV option.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,27 +23,38 @@ class ApplicationController < ActionController::Base
   def export_alumni  
 
     @all = Alum.find_by_sql("
-      SELECT *, degrees.id as deg_id
+      SELECT *, alums.id as alu_id, degrees.id as deg_id, records.id as rec_id
       FROM alums 
       LEFT OUTER JOIN degrees ON alums.id = degrees.alum_id
       LEFT OUTER JOIN degree_types ON degrees.degree_type_id = degree_types.id
+      LEFT OUTER JOIN records ON alums.id = records.alum_id
+      LEFT OUTER JOIN careers ON records.career_id = careers.id
     ") 
 
     csv_string = CSV.generate do |csv|   
       csv << [
-        "id", "first_name", "middle_name", "last_name", "sex", "dob", "mq_id", "linked_in", "twitter", "facebook", "degree_type_name", "graduation_year", "supervisor/s"]
+        "id", "first_name", "middle_name", "last_name", "sex", "dob", "mq_id", "linked_in", "twitter", "facebook", "degree_type_name", "graduation_year", "supervisor/s", "last_position_title", "last_career_name", "last_career_type"]
 
       @all.each do |i|
+
         sups = DegreesUser.where("degree_id IS ?", i.deg_id).map(&:user_id)
         sups = User.where("id IN (?)", sups).map { |s| "#{s.name} #{s.surname}" }.join(';')
-        puts sups
+
+        career_name = ""
+        career_type = ""
+        recs = Record.where("id IS ?", i.rec_id).last
+        if recs.present?
+          recs = Career.where("id IS ?", recs.career_id)
+          career_name = recs.map(&:career_name)[0]
+          career_type = recs.map(&:career_type)[0]
+        end
 
         if i.graduation_year.present?
           i.graduation_year = Date.parse(i.graduation_year).strftime("%Y")
         end
 
         csv << [
-          i.id, i.first_name, i.middle_name, i.last_name, i.sex, i.dob, i.mq_id, i.linked_in, i.twitter, i.facebook, i.degree_type_name, i.graduation_year, sups]
+          i.alu_id, i.first_name, i.middle_name, i.last_name, i.sex, i.dob, i.mq_id, i.linked_in, i.twitter, i.facebook, i.degree_type_name, i.graduation_year, sups, i.position_title, career_name, career_type]
         end 
     end 
     send_data csv_string, 
